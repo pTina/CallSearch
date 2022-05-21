73fa3797b220dde7ca0243c5377d721e0a272af3
gaeun <gaeun2937@gmail.com>
2022-05-21 23:27:27 +0900

    카카오내비 연동 map.js

--- js/map.js
+++ js/map.js
@@ -3,67 +3,48 @@
 // 도메인 등록
 // https://developers.kakao.com/docs/latest/ko/getting-started/app#platform
 
-// const REST_API_KEY = 'a296f869f92b1a4919ba690e84991602';
+const REST_API_KEY = 'a296f869f92b1a4919ba690e84991602';
 
-// const xhr = new XMLHttpRequest();
-// const url = `apis-navi.kakaomobility.com`;
-// const info = `origin=127.11015314141542,37.39472714688412&destination=127.10824367964793,37.401937080111644&waypoints=&priority=RECOMMEND&car_fuel=GASOLINE&car_hipass=false&alternatives=false&road_details=false`;
+const URL = `https://apis-navi.kakaomobility.com/v1/priority=RECOMMEND&car_type=1&car_fuel=GASOLINE`;
+// const INFO = `origin=127.11015314141542,37.39472714688412&destination=127.10824367964793%2C37.401937080111644`;
 
-// xhr.open('GET', url+info);
-// xhr.setRequestHeader('Authorization', `KakaoAK ${REST_API_KEY}"`);
-// console.log(xhr);
+// origin
+// 127.11015314141542,37.39472714688412
+// destination
+// 127.10824367964793,37.401937080111644
 
-
-
-
-// $.ajax({
-//     type: 'GET',
-//     url: url,
-//     // dataType: 'json',
-//     beforeSend: function (xhr) {
-//         xhr.setRequestHeader("Authorization", "KakaoAK${REST_API_KEY}");
-//     },
-//     error: function(xhr, status, error){
-//         alert(error); 
-//     },
-//     success : function(json){
-//         alert(json)
-//     },
-
-
-// })
-
-
-
-// xhr.open('GET', url);
-// xhr.onreadystatechange = function () {
-//     console.log(tthis.readyState);
-//     if (this.readyState == 4) {
-//         console.log(this.responseText);
-//         // console.log('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
-//     }
-// };
-
-
-
-// origin=127.11015314141542,37.39472714688412&destination=127.10824367964793,37.401937080111644&waypoints=&priority=RECOMMEND&car_fuel=GASOLINE&car_hipass=false&alternatives=false&road_details=false
-
-
-// Kakao.init(JAVASCRIPT_KEY);
-// const response = Kakao.isInitialized();
-// if(response){
-//     const Navi = Kakao.Navi;
+// const option = {
+//     method : 'GET',
+//     headers : new Headers({
+//         "Authorization": `KakaoAK ${REST_API_KEY}`
+//     }),
+//     mode: 'no-cors',
+//     body: JSON.stringify({
+//         origin : '127.11015314141542,37.39472714688412',
+//         destination : '127.10824367964793,37.401937080111644'
+//     })
 // }
 
+// fetch(URL)
+//     .then((res)=>{
+//         res
+//     })
 
-// $.ajax({
-//     url: 'https://apis-navi.kakaomobility.com/v1/directions?origin=127.11015314141542,37.39472714688412&destination=127.10824367964793,37.401937080111644&waypoints=&priority=RECOMMEND&car_fuel=GASOLINE&car_hipass=false&alternatives=false&road_details=false"',
-//     type:'GET',
-//     headers: {'Authorization' : Auth_KEY},
-//      success:function(data){
-//         console.log(data);
-//      },
-//      error : function(e){
-//         console.log(e);
-//      }
-// });
+// console.log(JSON.stringify({origin : '127.11015314141542,37.39472714688412',
+//          destination : '127.10824367964793,37.401937080111644'}))
+
+const url2 = 'https://apis-navi.kakaomobility.com/v1/directions?origin=127.11015314141542,37.39472714688412&destination=127.10824367964793,37.401937080111644&waypoints=&priority=RECOMMEND&car_fuel=GASOLINE&car_hipass=false&alternatives=false&road_details=false';
+const req = new Request(url2, {
+    method: "GET",
+    headers: new Headers({ 
+        // 'Content-Type': 'application/json',
+        "Authorization": `KakaoAK ${REST_API_KEY}`
+    })
+});
+
+fetch(req)
+    .then(response => response.json())
+    .then((data)=>{
+        console.log(data);
+    })
+    .catch(err => console.log(err));
