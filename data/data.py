f9cd221cacb1808b7292764ded340b5219f05d49
gaeun <gaeun2937@gmail.com>
2022-05-31 01:03:58 +0900

    data.txt => data.json, json파일 읽기

--- data/data.py
+++ data/data.py
@@ -1,4 +1,23 @@
-f = open("data.txt", 'r', encoding="utf-8")
-data = f.read()
-print(data)
-f.close()
+import os
+import json
+# 현재 디렉토리 경로 가져옴
+CUR_PATH = os.path.dirname(__file__)
+
+filePath = f'{CUR_PATH}/data.json'
+
+with open(filePath, 'r', encoding="utf8", errors='ignore') as file:
+    data = file.read()
+    # loads() => 문자열을 읽을 때 사용
+    # load() => 파일을 읽을 때 사용
+    myJSON = json.loads(data)
+
+items = myJSON['items']
+
+# 관내운행지역 insideOpratArea
+# 관외운행지역 outsideOpratArea
+# 이용요금 useCharge
+chargeList = [ x['useCharge'] for x in items]
+# useCharge = dict(zip(range(1, len(chargeList) + 1), chargeList))
+# print(useCharge)
+# with open(f'{CUR_PATH}/useCharge.txt','w',encoding='utf8') as file:
+#     file.write(str(useChargeList))
